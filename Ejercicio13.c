424d80073261109d5c815244e869ee313bb1bf5f
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2023-10-18 23:15:41 -0400

    Corregido para que el usuario solo pueda ingresar numeros positivos.

--- Ejercicio13.c
+++ Ejercicio13.c
@@ -8,12 +8,18 @@ int main(void)
     printf("Ingrese un numero.\n");
     scanf("%d", &numero);
 
+    while (numero < 0)
+    {
+        printf("Ingrese un numero positivo por favor.\n");
+        scanf("%d", &numero);
+    }
+
     for (int i = 1; i <= numero; i++)
     {
         if (i % 5 == 0)
         {
-            contador++ ;
-        printf("%d.\n",i);
+            contador++;
+            printf("%d.\n", i);
         }
     }
     printf("Entre 1 y el numero %d existen %d multiplos de 5 que son los mostrados anteriormente.\n", numero, contador);
