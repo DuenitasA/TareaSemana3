bb086128cd36dc8a1e4208f934596b7990dd0b79
Alejandro Duenas <alejandro.duenas@udla.edu.ec>
2023-10-18 22:39:27 -0400

    Finalmente tenemos la matriz completa y se imprimen correctamente todas las columnas

--- Ejercicio49.c
+++ Ejercicio49.c
@@ -11,20 +11,25 @@ int main(void)
         {
             if (j == 0)
             {
-                matriz[i][j] = i+1;
-            }else if (j == 1)
-            {
-                matriz[i][j] = (i/3)+1;
+                matriz[i][j] = i + 1;
             }
-            
+            else if (j == 1)
+            {
+                matriz[i][j] = (i / 3) + 1;
+            }
+            else
+            {
+
+                matriz[i][j] = (i % 3) + 1; 
+            }
         }
     }
 
     for (int i = 0; i < 9; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 3; j++)
         {
-            printf("%d", matriz[i][j]);
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
